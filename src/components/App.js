2dae83c2238580dc88c8c33793c2bd6f5404012a
Sarah Thiery <sarahvthiery@gmail.com>
2021-04-14 16:21:41 -0400

    Add cart component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import logo from '../logo.svg';
 import '../App.css';
 import Header from "./Header";
 import ShopContainer from "./ShopContainer";
+import Cart from "./Cart";
 
 function App() {
   return (
     <div>
     <Header/>
     <ShopContainer/>
+    <Cart/>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
