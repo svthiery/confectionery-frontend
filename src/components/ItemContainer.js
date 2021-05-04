c306eca420eefe38ad90c732f7ee37c729c9c13c
Sarah Thiery <sarahvthiery@gmail.com>
2021-05-04 16:34:21 -0400

    Finish flavor filter functionality

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,17 +1,23 @@
-
 import ItemTile from "./ItemTile";
 
-function ItemContainer({ allItems, viewedItem, setViewedItem }) {
-
-    const itemsList = allItems.map(item => {
-        return (
-            <ItemTile key={item.id} id={item.id} item={item} name={item.name} primary_img={item.primary_img} alt_img={item.alt_img} price={item.price} viewedItem={viewedItem} setViewedItem={setViewedItem}/>
-        )
-    })
-
+function ItemContainer({ items, viewedItem, setViewedItem }) {
+  const itemsList = items.map((item) => {
     return (
-        <div className="item-container col-8">{itemsList}</div>
+      <ItemTile
+        key={item.id}
+        id={item.id}
+        item={item}
+        name={item.name}
+        primary_img={item.primary_img}
+        alt_img={item.alt_img}
+        price={item.price}
+        viewedItem={viewedItem}
+        setViewedItem={setViewedItem}
+      />
     );
+  });
+
+  return <div className="item-container col-8">{itemsList}</div>;
 }
 
 export default ItemContainer;
--- src/components/ShopContainer.js
+++ src/components/ShopContainer.js
@@ -1,51 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sort from "./Sort";
 import FilterContainer from "./FilterContainer";
 import ItemContainer from "./ItemContainer";
 
 function ShopContainer({ allItems, viewedItem, setViewedItem }) {
+  const flavorList = [
+    "fruit",
+    "cookies and cream",
+    "dark chocolate",
+    "chocolate",
+    "cookie",
+    "vanilla",
+    "peanut butter",
+    "red velvet",
+    "assorted",
+    "sour",
+    "biscuits",
+    "brownie",
+    "popcorn",
+    "caramel",
+    "pretzel",
+    "hazelnut",
+    "sea salt",
+    "espresso",
+    "bacon",
+    "cotton candy",
+    "marshmallow",
+  ];
 
-    const flavorList = ["fruit", 
-        "cookies and cream", 
-        "dark chocolate", 
-        "chocolate", 
-        "cookie", 
-        "vanilla", 
-        "peanut butter", 
-        "red velvet", 
-        "assorted", 
-        "sour", 
-        "biscuits", 
-        "brownie", 
-        "popcorn", 
-        "caramel", 
-        "pretzel", 
-        "hazelnut", 
-        "sea salt", 
-        "espresso", 
-        "bacon", 
-        "cotton candy", 
-        "marshmallow"]
+  const [flavorsSelected, setFlavorsSelected] = useState(flavorList);
 
-    const [flavorsSelected, setFlavorsSelected] = useState([])
 
-    function handleCheckBox(flavor) {
-        const filteredFlavorList = [...flavorsSelected, flavor]
-        setFlavorsSelected(filteredFlavorList)
+  function handleCheckBox(flavor) {
+    if(flavorsSelected.length === 21) {
+        const filteredFlavorList = [flavor];
+        setFlavorsSelected(filteredFlavorList);
+    } else if(flavorsSelected.includes(flavor)){
+        const filteredFlavorList = flavorsSelected.filter(flavorItem => flavorItem !== flavor);
+        filteredFlavorList.length > 0 ? setFlavorsSelected(filteredFlavorList) : setFlavorsSelected(flavorList)
+    } else {
+        const filteredFlavorList = [...flavorsSelected, flavor];
+        setFlavorsSelected(filteredFlavorList);
     }
+  }
 
-    return (
-        <div className="row">
-            <div className="shop-container col-12">
-                <div className="shop-container-image">
-                    <h1 className="shop-heading">SHOP</h1>
-                </div>
-                {/* <img src={GummyWorms} className="background-img col-12"></img> */}
-                <FilterContainer flavorsSelected={flavorsSelected} setFlavorsSelected={setFlavorsSelected} flavorList={flavorList} handleCheckBox={handleCheckBox}/>
-                <ItemContainer allItems={allItems} viewedItem={viewedItem} setViewedItem={setViewedItem}/>
-            </div>
+  function getItemCategories(item) {
+      const categories = item.categories.map(category => {
+          return category.name.toLowerCase()
+      });
+      return categories
+  }
+
+  function matchesCat(item) {
+    const itemCats = getItemCategories(item)
+    for(let i = 0; i < itemCats.length; i++) {
+        for(let j = 0; j < flavorsSelected.length; j++) {
+            if(itemCats[i] === flavorsSelected[j]) {
+                return true
+            }
+        }
+    }
+  }
+
+  const filteredItems = allItems.filter(item => matchesCat(item)); 
+
+  return (
+    <div className="row">
+      <div className="shop-container col-12">
+        <div className="shop-container-image">
+          <h1 className="shop-heading">SHOP</h1>
         </div>
-    );
+        {/* <img src={GummyWorms} className="background-img col-12"></img> */}
+        <FilterContainer
+          flavorsSelected={flavorsSelected}
+          setFlavorsSelected={setFlavorsSelected}
+          flavorList={flavorList}
+          handleCheckBox={handleCheckBox}
+        />
+        <ItemContainer
+          items={filteredItems}
+          viewedItem={viewedItem}
+          setViewedItem={setViewedItem}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ShopContainer;
