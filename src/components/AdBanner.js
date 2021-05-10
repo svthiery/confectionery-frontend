f1518ac92bdc3843a8f87521810cc050a5dad5fe
Sarah Thiery <sarahvthiery@gmail.com>
2021-05-10 17:09:19 -0400

    Update favicon image

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -1,7 +1,7 @@
 function AdBanner() {
     return (
         <div className="row">
-            <div className="ad-banner col-12">AdBanner</div>
+            <div className="ad-banner col-12">Hi! I'm Sarah Kain Thiery, a Full-Stack Software Engineer. This site is under construction. Please visit my <a href="https://github.com/svthiery/confectionery-frontend">GitHub</a> profile for further examples of my work.</div>
         </div>
     );
 }
