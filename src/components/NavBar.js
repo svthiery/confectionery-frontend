d7b901aa188a57a921bb44f39d9e6517f8f90a86
Sarah Thiery <sarahvthiery@gmail.com>
2021-05-31 12:10:22 -0400

    Add nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
 function NavBar() {
-    return (
-        <div className="row">
-            <div className="nav-bar col-11">NavBar</div>
-        </div>
-    );
+  return (
+    <div className="row">
+      <nav className="nav-bar col-11">
+      <div class="subnav">
+    <button class="subnavbtn">Shop <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="#bring">Candy</a>
+      <a href="#deliver">Chocolate</a>
+      <a href="#package">Best Sellers</a>
+      <a href="#express">Sale</a>
+    </div>
+  </div>
+  <div class="subnav">
+    <button class="subnavbtn">About <i class="fa fa-caret-down"></i></button>
+    <div class="subnav-content">
+      <a href="https://www.linkedin.com/in/svk12/">LinkedIn</a>
+      <a href="#team">GitHub</a>
+      <a href="#careers">Email Me</a>
+    </div>
+  </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
