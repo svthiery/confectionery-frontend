1a2e014ff5f715549f4ea0fcf1cc55c494c831d7
Sarah Thiery <sarahvthiery@gmail.com>
2021-05-31 12:21:55 -0400

    Add navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,27 @@ function NavBar() {
   return (
     <div className="row">
       <nav className="nav-bar col-11">
-      <div class="subnav">
-    <button class="subnavbtn">Shop <i class="fa fa-caret-down"></i></button>
-    <div class="subnav-content">
-      <a href="#bring">Candy</a>
-      <a href="#deliver">Chocolate</a>
-      <a href="#package">Best Sellers</a>
-      <a href="#express">Sale</a>
-    </div>
-  </div>
-  <div class="subnav">
-    <button class="subnavbtn">About <i class="fa fa-caret-down"></i></button>
-    <div class="subnav-content">
-      <a href="https://www.linkedin.com/in/svk12/">LinkedIn</a>
-      <a href="#team">GitHub</a>
-      <a href="#careers">Email Me</a>
-    </div>
-  </div>
+        <div class="subnav subnav-shop">
+          <button class="subnavbtn">
+            SHOP <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="#bring">Candy</a>
+            <a href="#deliver">Chocolate</a>
+            <a href="#package">Best Sellers</a>
+            <a href="#express">Sale</a>
+          </div>
+        </div>
+        <div class="subnav subnav-about">
+          <button class="subnavbtn">
+            ABOUT <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="subnav-content">
+            <a href="https://www.linkedin.com/in/svk12/">LinkedIn</a>
+            <a href="#team">GitHub</a>
+            <a href="#careers">Email Me</a>
+          </div>
+        </div>
       </nav>
     </div>
   );
