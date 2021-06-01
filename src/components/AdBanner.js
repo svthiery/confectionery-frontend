6736a8e0d901dc57b003055b1567ba3c96a00698
Sarah Thiery <sarahvthiery@gmail.com>
2021-06-01 08:47:14 -0400

    Fix ad banner height

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -1,7 +1,9 @@
 function AdBanner() {
     return (
         <div className="row">
-            <div className="ad-banner col-12">Hi! I'm Sarah Kain Thiery, a Full-Stack Software Engineer. This site is under construction. Please visit my <a href="https://github.com/svthiery/confectionery-frontend">GitHub</a> for further examples of my work.</div>
+            <div className="ad-banner col-12">
+                <p className="ad-banner-content">Hi! I'm Sarah Kain Thiery, a Full-Stack Software Engineer. This site is under construction. Please visit my <a href="https://github.com/svthiery/confectionery-frontend">GitHub</a> for further examples of my work.</p>
+            </div>
         </div>
     );
 }
