12cb197fdff1ae51eb3c810c9bfc1c7d9539f541
Sarah Thiery <sarahvthiery@gmail.com>
2021-06-03 16:03:21 -0400

    Update ad banner

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -2,7 +2,7 @@ function AdBanner() {
     return (
         <div className="row">
             <div className="ad-banner col-12">
-                <p className="ad-banner-content">Hi! I'm Sarah Kain Thiery, a Full-Stack Software Engineer. This site is under construction. Please visit my <a href="https://github.com/svthiery/confectionery-frontend">GitHub</a> for further examples of my work.</p>
+                <p className="ad-banner-content">Hi! I'm Sarah Kain Thiery, a Full-Stack Software Engineer. Please visit my <a href="https://github.com/svthiery/confectionery-frontend">GitHub</a> for further examples of my work.</p>
             </div>
         </div>
     );
