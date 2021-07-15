26ad688733753cd52612c15ba801aace9332af22
Sarah Thiery <sarahvthiery@gmail.com>
2021-07-15 16:35:26 -0700

    Update user value

--- src/components/App.js
+++ src/components/App.js
@@ -55,7 +55,7 @@ function App() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ user_id: 1 }),
+      body: JSON.stringify({ user_id: currentUser ? currentUser.id : 0 }),
     })
       .then((response) => response.json())
       .then((data) => {
